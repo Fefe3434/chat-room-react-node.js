7720207f35b142d96aa63a878b7a45bc63900f25
Ferdaouss Adlani <adlani.ferdaous@gmail.com>
2025-01-16 14:09:28 +0100

    router basename  updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
 
 const App = () => (
-    <Router>
-        <Routes basename="/chat-room-react-node.js">
+    <Router basename="/chat-room-react-node.js">
+        <Routes >
             <Route path="/" element={<Join />} />
             <Route path="/chat" element={<Chat />} />
         </Routes>
